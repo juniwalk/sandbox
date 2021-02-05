d37a061492644e60dd8907af64e8d7c542da8218
juniwalk <juniwalk@outlook.cz>
2021-02-05 09:52:04 +0100

    Updated default user email

--- db/2019/01/Version20190109141903.php
+++ db/2019/01/Version20190109141903.php
@@ -17,7 +17,7 @@ final class Version20190109141903 extends AbstractMigration
 
         $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, name VARCHAR(64) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(64) DEFAULT NULL, role VARCHAR(32) NOT NULL, sign_up TIMESTAMP(0) WITH TIME ZONE DEFAULT now() NOT NULL, sign_in TIMESTAMP(0) WITH TIME ZONE DEFAULT NULL, is_active BOOLEAN DEFAULT \'true\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
-        $this->addSql('INSERT INTO "user" (name, email, role) VALUES (\'Administrator\', \'info@dpoint.cz\', \'admin\')');
+        $this->addSql('INSERT INTO "user" (name, email, role) VALUES (\'Administrator\', \'info@web7.cz\', \'admin\')');
     }
 
     public function down(Schema $schema) : void
